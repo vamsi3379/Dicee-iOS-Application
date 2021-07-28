bb92fe7e1cc61c73df2afa6466ce1c71031c09f4
vamsi <vamsiraju835@gmail.com>
2021-07-28 10:01:44 +0530

    Added all the  dice images  and  connected dice images and the roll button to the view controller.swift and mains tory board

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        
     }
 
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
+        
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceSix") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceTwo")][rightDiceNumber]
+        rightDiceNumber = rightDiceNumber + 1
+        
+    }
+    
 }
 
