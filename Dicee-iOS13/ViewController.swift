e084767a399a515c6e22b6022245e6c4a2857af1
Vamsi Pachamatla <vamsiraju835@gmail.com>
2021-07-28 11:09:15 +0530

    Fully Functionable Dice application(used int.random to choose the random dice images)

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,13 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 0
-    var rightDiceNumber = 0
+    var leftDiceNumber = Int.random(in: 0...5)
+    var rightDiceNumber = Int.random(in: 0...5)
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
         
         
     }
@@ -28,11 +30,14 @@ class ViewController: UIViewController {
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        var leftDiceNumber = Int.random(in: 0...5)
+        var rightDiceNumber = Int.random(in: 0...5)
+        
         diceImageView1.image = [#imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceTwo") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
+        
         
         diceImageView2.image = [#imageLiteral(resourceName: "DiceFour") , #imageLiteral(resourceName: "DiceSix") , #imageLiteral(resourceName: "DiceFive") , #imageLiteral(resourceName: "DiceOne") , #imageLiteral(resourceName: "DiceThree") , #imageLiteral(resourceName: "DiceTwo")][rightDiceNumber]
-        rightDiceNumber = rightDiceNumber + 1
+        
         
     }
     
